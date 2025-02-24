56340b308b652565aa5ecce7d37551c39f75fb59
Praxedis-Calleja <65052872+Praxedis-Calleja@users.noreply.github.com>
2025-02-24 17:36:47 -0500

    AA 1.3 Ejercicio

---
+++ 1.3 Ejercicios/servers/index.js
@@ -0,0 +1,26 @@
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+
+
+app.get('/', (req, res) => {
+    
+  res.send('<h1>Bienvenido a mi página web</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>Acerca de</h1>');
+});
+
+app.use ((req, res) => 
+{
+  res.status(404).send('<h1>404 Not Found</h1>');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
